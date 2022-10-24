9c0fda2d86808194f5377c63dabb520ba872b65d
Sourjadip Pramanik <84491010+Sourja99@users.noreply.github.com>
2022-10-24 13:31:34 +0530

    Add files via upload

---
+++ RR.py
@@ -0,0 +1,94 @@
+def findWaitingTime(processes, n, bt, wt, quantum):
+    rem_bt = [0] * n
+ 
+    for i in range(n):
+        rem_bt[i] = bt[i]
+    t = 0 # Cur
+ 
+    while(1):
+        done = True
+ 
+        for i in range(n):
+             
+            if (rem_bt[i] > 0) :
+                done = False 
+                 
+                if (rem_bt[i] > quantum) :
+                    t += quantum
+                    rem_bt[i] -= quantum
+                 
+                else:
+                    t = t + rem_bt[i]
+                    wt[i] = t - bt[i]
+                    rem_bt[i] = 0
+                 
+        if (done == True):
+            break
+             
+
+def findTurnAroundTime(processes, n, bt, wt, tat):
+     
+    for i in range(n):
+        tat[i] = bt[i] + wt[i]
+ 
+ 
+def findavgTime(processes, n, bt, quantum):
+    wt = [0] * n
+    tat = [0] * n
+ 
+    findWaitingTime(processes, n, bt,
+                         wt, quantum)
+ 
+    findTurnAroundTime(processes, n, bt,
+                                wt, tat)
+ 
+    print("Processes    Burst Time     Waiting",
+                     "Time    Turn-Around Time")
+    total_wt = 0
+    total_tat = 0
+    for i in range(n):
+ 
+        total_wt = total_wt + wt[i]
+        total_tat = total_tat + tat[i]
+        print(" ", i + 1, "\t\t", bt[i],
+              "\t\t", wt[i], "\t\t", tat[i])
+ 
+    print("\nAverage waiting time = %.5f "%(total_wt /n) )
+    print("Average turn around time = %.5f "% (total_tat / n))
+     
+# Driver code
+if __name__ =="__main__":
+
+    proc = []
+    burst_time = []
+    length = int(input("Enter number of process: "))
+  
+    for i in range(0, length):
+        ele = int(input('Enter process id : '))  
+        burst = int(input('Enter burst time : '))
+        proc.append(ele) 
+        burst_time.append(burst)
+
+    n = len(proc)    
+     
+    # Process id's
+    #proc = [1, 2, 3]
+    #n = 3
+
+    
+    #length = int(input("Enter number of process: "))
+  
+    # for i in range(0, length):
+    #     ele = int(input('Enter process id : '))  
+    #     proc.append(ele) 
+
+    # burst_time = int(input("Enter burst time: "))
+ 
+    # Burst time of all processes
+    #burst_time = [10, 5, 8]
+ 
+    # Time quantum
+    #quantum = 2;
+    quantum = int(input("Enter the Quantum time: "))
+    findavgTime(proc, n, burst_time, quantum)
+ 
